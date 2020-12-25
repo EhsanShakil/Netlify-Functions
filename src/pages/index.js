5f271078ff9862e8d81064ddd7d520de7adde6ca
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-25 23:44:29 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(".netlify/functions/hello");
+      const details = await response.json();
+      setData(details);
+    };
+    fetchData();
+    console.log(data);
+  }, []);
+
   return (
     <div>
       <h1>Hello World From Gatsby</h1>
+      <h1>{data.message}</h1>
     </div>
   );
 };
